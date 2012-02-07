f31bb646f7977f3937d3db9769f741f5e9784ce4
markadamson83 <markadamson83@37e19c6a-44a1-a3f6-e999-e6f4f291b044>
2012-02-07 19:20:38 +0000

    Initial checkin

---
+++ trunk/src/com/markadamson/snakemon/SnakeMonSettings.java
@@ -0,0 +1,35 @@
+package com.markadamson.snakemon;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SnakeMonSettings extends PreferenceActivity
+    implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        getPreferenceManager().setSharedPreferencesName(
+                SnakeMonService.SHARED_PREFS_NAME);
+        addPreferencesFromResource(R.xml.snakemon_settings);
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(
+                this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(
+                this);
+        super.onDestroy();
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+            String key) {
+    }
+}
