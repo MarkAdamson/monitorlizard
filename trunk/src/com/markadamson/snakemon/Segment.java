8bce1acf18d87dc88aa68c789609dcd8f200d82c
markadamson83 <markadamson83@37e19c6a-44a1-a3f6-e999-e6f4f291b044>
2012-02-16 16:37:43 +0000

    copyright and license details

--- trunk/src/com/markadamson/snakemon/Segment.java
+++ trunk/src/com/markadamson/snakemon/Segment.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2012 Mark Adamson
+ *  
+ *  This file is part of Monitor Lizard.
+ * 
+ *  Monitor Lizard is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Monitor Lizard is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Monitor Lizard.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.markadamson.snakemon;
 
 import android.graphics.Canvas;
