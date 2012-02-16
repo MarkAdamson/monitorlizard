74d5a5682b9921a9659ccc5ad4a5a81152c3fa33
markadamson83 <markadamson83@37e19c6a-44a1-a3f6-e999-e6f4f291b044>
2012-02-16 16:17:26 +0000

    CPU, collisions, pathfinding and snake death

--- src/com/markadamson/snakemon/Segment.java
+++ src/com/markadamson/snakemon/Segment.java
@@ -24,6 +24,22 @@ public class Segment
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
 	}
+	
+	boolean willCollide(Segment s)
+	{
+		int Xdest = Xpos;
+		int Ydest = Ypos;
+		switch(direction)
+		{
+		case 0: Xdest--; break;
+		case 1: Ydest--; break;
+		case 2: Xdest++; break;
+		case 3: Ydest++; break;
+		}
+		
+		if(Xdest==s.getXpos() && Ydest==s.getYpos()) return true;
+		else return false;
+	}
 
 	int getXpos()
 	{
@@ -33,6 +49,20 @@ public class Segment
 	int getYpos()
 	{
 		return Ypos;
+	}
+	
+	int getXdest()
+	{
+		if(direction==0) return Xpos-1;
+		else if(direction==2) return Xpos+1;
+		else return Xpos;
+	}
+	
+	int getYdest()
+	{
+		if(direction==1) return Ypos-1;
+		else if(direction==3) return Ypos+1;
+		else return Ypos;
 	}
 	
 	void Move()
@@ -70,5 +100,13 @@ public class Segment
 	public int getDirection() {
 		// TODO Auto-generated method stub
 		return direction;
+	}
+
+	public void setXpos(int i) {
+		Xpos = i;
+	}
+
+	public void setYpos(int i) {
+		Ypos = i;
 	}
 }
